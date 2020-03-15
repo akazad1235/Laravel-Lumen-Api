07675e409dcd8bb81ed6a3f720a7b725e6264138
akazad1235 <akazad914@gmail.com>
2020-03-15 13:07:08 +0600

    page redirect

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
-$router->get('/{name}/{age}', 'testController@my');
+//$router->get('/{name}/{age}', 'testController@my');
+$router->get('/first', 'myController@first');
+$router->get('/second', 'myController@second');
+$router->get('/third', 'myController@third');
 
 
 
